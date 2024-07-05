8c4e8b13f5588205c75486dc6c1c33bf073af620
monower <sadafahmed720@gmail.com>
2024-07-06 00:37:33 +0600

    added typescript

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import dynamic from "next/dynamic";
 const HeroAnimation = dynamic(() => import("./HeroAnimation"), {
   ssr: false,
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Home/Hero";
+import Skills from "./components/Home/Skills";
+import Experience from "./components/Home/Experience";
+import Projects from "./components/Home/Projects";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      <Experience />
+      <Projects />
+    </>
+  );
+}
